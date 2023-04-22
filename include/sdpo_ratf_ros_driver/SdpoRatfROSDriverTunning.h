63b3f66c21dbfde94e54c337a19217a6524b6d0a
Ricardo B. Sousa <sousa.ricardob@outlook.com>
2023-04-22 20:03:46 +0100

    Node to communicate directly with firmware for future tunniong

---
+++ include/sdpo_ratf_ros_driver/SdpoRatfROSDriverTunning.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <sdpo_ros_interfaces_hw/mot_enc_array.h>
+#include <sdpo_ros_interfaces_hw/mot_ref.h>
+#include <std_srvs/SetBool.h>
+
+#include "sdpo_ratf_ros_driver/Robot5dpoRatfTune.h"
+
+namespace sdpo_ratf_ros_driver {
+
+class SdpoRatfROSDriverTunning {
+ private:
+  ros::NodeHandle nh;
+
+  ros::Publisher pub_mot_enc_;
+  ros::Publisher pub_switch_;
+
+  ros::ServiceServer srv_solenoid_;
+
+  Robot5dpoRatfTune rob_;
+
+  std::string serial_port_name_;
+
+ public:
+  SdpoRatfROSDriverTunning();
+  ~SdpoRatfROSDriverTunning() = default;
+
+  void run();
+
+ private:
+  bool readParam();
+
+  void pubMotEnc();
+  void pubSwitch();
+
+  bool srvSolenoid(std_srvs::SetBool::Request& request,
+      std_srvs::SetBool::Response& response);
+};
+
+} // namespace sdpo_ratf_ros_driver
---
+++ src/sdpo_ratf_ros_driver/SdpoRatfROSDriverTunning.cpp
@@ -0,0 +1,101 @@
+#include "sdpo_ratf_ros_driver/SdpoRatfROSDriverTunning.h"
+
+#include <std_msgs/Bool.h>
+
+namespace sdpo_ratf_ros_driver {
+
+SdpoRatfROSDriverTunning::SdpoRatfROSDriverTunning() {
+  readParam();
+
+  rob_.setSerialPortName(serial_port_name_);
+  rob_.openSerial();
+  rob_.run = std::bind(&SdpoRatfROSDriverTunning::run, this);
+  rob_.init();
+
+  pub_mot_enc_ = nh.advertise<sdpo_ros_interfaces_hw::mot_enc_array>(
+      "motors_encoders", 1);
+  pub_switch_ = nh.advertise<std_msgs::Bool>("switch_state", 1);
+
+  srv_solenoid_ = nh.advertiseService("set_solenoid_state",
+      &SdpoRatfROSDriverTunning::srvSolenoid, this);
+}
+
+void SdpoRatfROSDriverTunning::run() {
+  ROS_INFO("Test");
+  pubMotEnc();
+  pubSwitch();
+}
+
+bool SdpoRatfROSDriverTunning::readParam() {
+  ros::NodeHandle nh_private("~");
+
+  for (auto& m : rob_.mot) {
+    nh_private.param<double>("encoder_res", m.encoder_res, 48.0);
+    nh_private.param<double>("gear_reduction", m.gear_reduction, 64.0);
+  }
+  nh_private.param<std::string>("serial_port_name", serial_port_name_,
+                                "/dev/ttyACM0");
+
+  auto print_is_default_param_set =
+      [&nh_private](const std::string& param_name) {
+    if (!nh_private.hasParam(param_name)) {
+      ROS_INFO("[sdpo_ratf_ros_driver] Parameter %s not set in the "
+               "parameter server (using default value)",
+               param_name.c_str());
+    }
+  };
+
+  print_is_default_param_set("encoder_res");
+  ROS_INFO("[sdpo_ratf_ros_driver] Encoder resolution: %lf (ticks/rev)",
+           rob_.mot[0].encoder_res);
+
+  print_is_default_param_set("gear_reduction");
+  ROS_INFO("[sdpo_ratf_ros_driver] Gear reduction ratio: %lf (n:1)",
+           rob_.mot[0].gear_reduction);
+
+  print_is_default_param_set("serial_port_name");
+  ROS_INFO("[sdpo_ratf_ros_driver] Serial port: %s", serial_port_name_.c_str());
+
+  return true;
+}
+
+void SdpoRatfROSDriverTunning::pubMotEnc() {
+  sdpo_ros_interfaces_hw::mot_enc_array msg;
+
+  msg.stamp = ros::Time::now();
+  msg.mot_enc_array_data.resize(4);
+
+  rob_.mtx_.lock();
+  for (int i = 0; i < 4; i++) {
+    msg.mot_enc_array_data[i].encoder_delta = rob_.mot[i].getEncTicksDeltaPub();
+    msg.mot_enc_array_data[i].ticks_per_rev =
+        rob_.mot[i].encoder_res * rob_.mot[i].gear_reduction;
+    msg.mot_enc_array_data[i].angular_speed = rob_.mot[i].w;
+  }
+  rob_.mtx_.unlock();
+
+  pub_mot_enc_.publish(msg);
+}
+
+void SdpoRatfROSDriverTunning::pubSwitch() {
+  std_msgs::Bool msg;
+
+  rob_.mtx_.lock();
+  msg.data = rob_.switch_state;
+  rob_.mtx_.unlock();
+
+  pub_switch_.publish(msg);
+}
+
+bool SdpoRatfROSDriverTunning::srvSolenoid(std_srvs::SetBool::Request& request,
+    std_srvs::SetBool::Response& response) {
+  rob_.mtx_.lock();
+  rob_.solenoid_state = request.data;
+  rob_.mtx_.unlock();
+
+  response.success = true;
+  response.message = "";
+  return true;
+}
+
+} // namespace sdpo_ratf_ros_driver
